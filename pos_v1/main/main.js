240977cdddd608842c619a980dac8af2689c4556
utlight <joker_e@163.com>
2017-03-23 20:58:44 +0800

    format code style

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -8,17 +8,61 @@ const printReceipt = (tags) => {
   printGoodsInfo(receiptInfo)
 }
 
-const printGoodsInfo = (receiptInfo) => {
-  let printInfo = `***<没钱赚商店>收据***`
-  for (let goodInfo of receiptInfo.cartItems) {
-    let goodInfoStr = `\n名称：${goodInfo.name}，数量：${goodInfo.count}${goodInfo.unit}，单价：${goodInfo.price.toFixed(2)}(元)，小计：${goodInfo.total.toFixed(2)}(元)`
-    printInfo += goodInfoStr
-  }
-  printInfo += `\n----------------------\n总计：${receiptInfo.allGoodsPrice.toFixed(2)}(元)
-节省：${receiptInfo.allGoodsSaved.toFixed(2)}(元)\n**********************`
-  console.log(printInfo)
+const getAllGoodsCount = (userBarcodes) => {
+  let allGoodsCount = []
+  userBarcodes.map(userBarcode => {
+    let {
+      barcode,
+      count
+    } = resolveUserBarcode(userBarcode)
+    let findResult = findByBarcode(allGoodsCount, barcode)
+    if (findResult == null) {
+      allGoodsCount.push({
+        barcode: barcode,
+        count: count
+      })
+    } else {
+      findResult.count += count
+    }
+  })
+  return allGoodsCount
 }
 
+const resolveUserBarcode = (userBarcode) => {
+  let [barcode, count] = userBarcode.split('-')
+  count = count == undefined ? 1 : parseFloat(count)
+  return {
+    barcode,
+    count
+  }
+}
+
+const findByBarcode = (array, barcode) => {
+  let searchResult = array.filter(a => a.barcode === barcode)
+  return searchResult.length > 0 ? searchResult[0] : null
+}
+
+const getAllGoodsInfo = (barcodeInfo) => {
+  let items = loadAllItems()
+  return barcodeInfo.map(goods => {
+    let searchItems = items.filter(item => item.barcode === goods.barcode)
+    searchItems[0].count = goods.count
+    return searchItems[0]
+  })
+}
+
+const getAllGoodsPromotion = (allGoodsInfoCount) => {
+  let promotions = loadPromotions()
+  return allGoodsInfoCount.map(goods => {
+    let findPromotion = promotions.filter(p => p.barcodes.includes(goods.barcode))
+    if (findPromotion.length > 0) {
+      goods.promotion_type = findPromotion[0].type
+    } else {
+      goods.promotion_type = ""
+    }
+    return goods
+  })
+}
 
 const countAllGoodsPrice = (allGoods) => {
   let allGoodsPrice = 0
@@ -55,59 +99,14 @@ const countGoodsPrice = (goods) => {
   }
 }
 
-const getAllGoodsInfo = (barcodeInfo) => {
-  let items = loadAllItems()
-  return barcodeInfo.map(goods => {
-    let searchItems = items.filter(item => item.barcode === goods.barcode)
-    searchItems[0].count = goods.count
-    return searchItems[0]
-  })
-}
-
-const getAllGoodsPromotion = (allGoodsInfoCount) => {
-  let promotions = loadPromotions()
-  return allGoodsInfoCount.map(goods => {
-    let findPromotion = promotions.filter(p => p.barcodes.includes(goods.barcode))
-    if (findPromotion.length > 0) {
-      goods.promotion_type = findPromotion[0].type
-    } else {
-      goods.promotion_type = ""
-    }
-    return goods
-  })
-}
-
-const getAllGoodsCount = (userBarcodes) => {
-  let allGoodsCount = []
-  userBarcodes.map(userBarcode => {
-    let {
-      barcode,
-      count
-    } = resolveUserBarcode(userBarcode)
-    let findResult = findByBarcode(allGoodsCount, barcode)
-    if (findResult == null) {
-      allGoodsCount.push({
-        barcode: barcode,
-        count: count
-      })
-    } else {
-      findResult.count += count
-    }
-  })
-  return allGoodsCount
-}
-
-const resolveUserBarcode = (userBarcode) => {
-  let [barcode, count] = userBarcode.split('-')
-  count = count == undefined ? 1 : parseFloat(count)
-  return {
-    barcode,
-    count
+const printGoodsInfo = (receiptInfo) => {
+  let printInfo = `***<没钱赚商店>收据***`
+  for (let goodInfo of receiptInfo.cartItems) {
+    let goodInfoStr = `\n名称：${goodInfo.name}，数量：${goodInfo.count}${goodInfo.unit}，单价：${goodInfo.price.toFixed(2)}(元)，小计：${goodInfo.total.toFixed(2)}(元)`
+    printInfo += goodInfoStr
   }
-}
-
-const findByBarcode = (array, barcode) => {
-  let searchResult = array.filter(a => a.barcode === barcode)
-  return searchResult.length > 0 ? searchResult[0] : null
+  printInfo += `\n----------------------\n总计：${receiptInfo.allGoodsPrice.toFixed(2)}(元)
+节省：${receiptInfo.allGoodsSaved.toFixed(2)}(元)\n**********************`
+  console.log(printInfo)
 }
 
